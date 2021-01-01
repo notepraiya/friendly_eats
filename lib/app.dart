fda6be79b47d0f4689ba6200e22e0e85ff25e00f
notepraiya <notepraiya@gmail.com>
2021-01-01 15:15:32 +0700

    runnable initialize friendly eats app

---
+++ lib/app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'home_page.dart';
+
+class FriendlyEatsApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,12 +8,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:friendly_eats/main.dart';
+import 'package:friendly_eats/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(FriendlyEatsApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
